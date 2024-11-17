999b69d8d177cfa9cfaaff9686c3301cf3079cb2
ErikaF <erikafebriyani_2226240050@mhs.mdp.ac.id>
2024-11-17 14:26:46 +0700

    Menambahkan
    Favorite Screen dan Second Screen

--- wisata_candi1/lib/main.dart
+++ wisata_candi1/lib/main.dart
@@ -23,3 +23,47 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+class MainSreen extends StatefulWidget { const MainSreen({super.key});
+@override
+State<MainSreen> createState() => _MainSreenState(); }
+class _MainSreenState extends State<MainSreen> {
+  // TODO: 1. Deklarasikan variabel
+  int _currentIndex = 0;
+  final List<Widget> _children = [ const HomeScreen(),
+    const SearchScreen(),
+    const FavoriteScreen(),
+    const ProfileScreen(), ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+// TODO: 2. Buat properti body berupa widget yang ditampilkan
+      body: _children[_currentIndex],
+// TODO: 3. Buat properti bottomNavigationBar dengan nilai Theme
+      bottomNavigationBar: Theme(
+// TODO: 4. Buat data dan child dari Theme
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.deepPurple[50], ),
+        child: BottomNavigationBar( currentIndex: _currentIndex, onTap: (index){
+          setState(() {
+            _currentIndex = index; });
+        },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: Colors.deepPurple,), label: 'Home',
+            ), BottomNavigationBarItem(
+              icon: Icon(Icons.search, color: Colors.deepPurple,),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite, color: Colors.deepPurple,), label: 'Favorite',
+            ), BottomNavigationBarItem(
+              icon: Icon(Icons.person, color: Colors.deepPurple,),
+              label: 'Profile',
+            ),
+          ],
+          selectedItemColor: Colors.deepPurple, unselectedItemColor: Colors.deepPurple[100], showUnselectedLabels: true,
+        ),
+      ),
+    );
+  }
+}
---
+++ wisata_candi1/lib/screens/Second_Screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Screen'), ),
+      body: Center(
+        child: Center(
+          child: ElevatedButton( onPressed: (){
+            Navigator.pop(context); },
+            child: const Text('Back to Home'), ),
+        ),
+      ),
+    );
+  }
+}
